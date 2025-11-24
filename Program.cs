c2b0c597b8e691a88c9b798b7e4f0501a7e83cb9
yehoki <patsenn2000@gmail.com>
2025-11-24 20:06:22 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Newtonsoft.Json;
+
+
+while (true)
+{
+    Console.WriteLine("Hello, World!");
+    var input = Console.ReadLine();
+    if (string.IsNullOrEmpty(input))
+        Console.WriteLine("Exiting Program, empty input");
+    if (Enum.TryParse(typeof(Patterns), input, out var pattern))
+    {
+        // Get pattern
+    }
+    else
+    {
+        Console.WriteLine("Invalid input, please try again");
+    }
+}
+
+
+public enum Patterns
+{
+    Factory,
+    AbstractFactory,
+    Builder,
+}
