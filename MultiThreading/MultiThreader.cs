b3580a6999dee93c87aeafcaed30180f90230891
yehoki <patsenn2000@gmail.com>
2025-11-28 08:26:14 +0000

    Setting up delayed web server response example

--- MultiThreading/MultiThreader.cs
+++ MultiThreading/MultiThreader.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using static System.Threading.Thread;
 
 namespace design_patterns.MultiThreading;
@@ -22,5 +23,51 @@ public class MultiThreader
         }
 
         return segmentSum;
+    }
+
+    public static void SetupWebServer()
+    {
+        Console.WriteLine("Service is running, please type 'exit' to stop");
+        // Setting up Request queue
+        ConcurrentQueue<string?> requestQueue = new();
+        Thread monitoringThread = new Thread(MonitorQueue);
+        monitoringThread.Start(requestQueue);
+        while (true)
+        {   
+            string? input = Console.ReadLine();
+            if (input?.ToLower() == "exit")
+                break;
+
+            requestQueue.Enqueue(input);
+        }
+    }
+
+    private static void MonitorQueue(object? o)
+    {
+        if (o is not ConcurrentQueue<string?> requestQueue)
+        {
+            Console.WriteLine("Incorrect Implementation - Queue monitor expecting Concurrent queue");
+        }
+        else
+        {
+            while (true)
+            {
+                if (!requestQueue.IsEmpty)
+                {
+                    requestQueue.TryDequeue(out string? result);
+                    Thread processingThread = new(() => ProcessRequest(result));
+                    processingThread.Start();
+                }
+
+                // Ensuring loop here doesn't explode from too many requests and take up too much CPU
+                Thread.Sleep(100);
+            }
+        }
+    }
+
+    private static void ProcessRequest(string? input)
+    {
+        Thread.Sleep(2000);
+        Console.WriteLine($"Processed input {input}");
     }
 }
