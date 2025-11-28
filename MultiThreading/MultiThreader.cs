f51647e551949810d47100974d941365f205c2b5
yehoki <patsenn2000@gmail.com>
2025-11-28 14:15:20 +0000

    Adding a mutex setup for multi process apps

--- MultiThreading/MultiThreader.cs
+++ MultiThreading/MultiThreader.cs
@@ -116,4 +116,45 @@ public class MultiThreader
             }
         }
     }
+
+    public static void SetupMutexFileExample()
+    {
+        string filePath = "counter.txt";
+        using (var mutex = new Mutex(false, $"GlobalFileMutex:{filePath}"))
+            for (int i = 0; i < 10000; i++)
+            {
+                mutex.WaitOne();
+                try
+                {
+                    int counter = ReadCounter(filePath);
+                    counter++;
+                    WriteCounter(filePath, counter);
+                }
+                finally
+                {
+                    mutex.ReleaseMutex();
+                }
+            }
+
+        Console.WriteLine("Process finished");
+    }
+
+    private static int ReadCounter(string filePath)
+    {
+        using (var stream = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Read, FileShare.ReadWrite))
+        using (var reader = new StreamReader(stream))
+        {
+            string content = reader.ReadToEnd();
+            return string.IsNullOrEmpty(content) ? 0 : int.Parse(content);
+        }
+    }
+
+    private static void WriteCounter(string filePath, int counter)
+    {
+        using (var stream = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write, FileShare.ReadWrite))
+        using (var writer = new StreamWriter(stream))
+        {
+            writer.Write(counter);
+        }
+    }
 }
